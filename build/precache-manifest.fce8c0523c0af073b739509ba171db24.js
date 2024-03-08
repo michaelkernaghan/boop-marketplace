d965a890a9f7d50cee37da1385e30ba6d6c9cb92
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2024-03-08 08:08:55 -0800

    initial deploy

---
+++ build/precache-manifest.fce8c0523c0af073b739509ba171db24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d71c4d39a60e702def8",
+    "url": "/static/css/main.e1fb7a29.chunk.css"
+  },
+  {
+    "revision": "0d71c4d39a60e702def8",
+    "url": "/static/js/main.0d71c4d3.chunk.js"
+  },
+  {
+    "revision": "bcca58e69e0571faa874",
+    "url": "/static/css/1.4d9259cc.chunk.css"
+  },
+  {
+    "revision": "bcca58e69e0571faa874",
+    "url": "/static/js/1.bcca58e6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "7ea6a3c9b2b78dde0e61c96dd7ae5682",
+    "url": "/index.html"
+  }
+];
---
+++ truffle-config.js
@@ -0,0 +1,34 @@
+require('babel-register');
+require('babel-polyfill');
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = "quarter gift medal brother eagle soon action morning pull under mom violin";
+
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    etherlinkTestnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://node.ghostnet.etherlink.com`),
+      network_id: 128123, // Chain ID for Etherlink Testnet
+      gas: 5500000, // Gas limit used for transactions
+      gasPrice: 21000000000,
+      timeoutBlocks: 200, // Number of blocks before a deployment times out
+      skipDryRun: true // Skip dry run before migrations? Up to you
+    }
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
